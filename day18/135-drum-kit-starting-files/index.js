c51b020a7ffcdb5f0a9bcd1e9aa60c381d113beb
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2025-01-07 19:39:15 -0300

    day18

--- day18/135-drum-kit-starting-files/index.js
+++ day18/135-drum-kit-starting-files/index.js
@@ -1,10 +1,8 @@
 function handleClick() {
-  const audio = new Audio("crash.mp3");
+  const audio = new Audio("sounds/crash.mp3");
   audio.play();
 }
 
-// for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-//   document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-// }
-
-document.querySelector(".drum").addEventListener("click", handleClick);
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}

297d551ccbff3fecfd1f6b019741ee904e551495
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2025-01-22 12:33:54 -0300

    day24

--- day17/index.js
+++ day17/index.js
@@ -13,6 +13,7 @@ if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 1 Wins";
 } else if (randomNumber1 < randomNumber2) {
   document.querySelector("h1").innerHTML = "Player 2 Wins";
-} else if (randomNumber1 == randomNumber2) {
-  document.querySelector("h1").innerHTML = "Draw";
+}
+else if (randomNumber1 == randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draws";
 }
--- day24/201-express-server/index.js
+++ day24/201-express-server/index.js
@@ -17,6 +17,14 @@ app.get("/", (req, res) => {
   res.send("<h1> Hello World! <h1>");
 });
 
+app.get("/contact", (req, res) => {
+  res.send("<h1>My Contact</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>About Me</h1>");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
